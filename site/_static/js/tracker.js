0608f480d66867159e0014fabf801e39d6f20541
Max Morokko <mmorokko@gocardless.com>
2022-01-23 11:33:51 +0000

    new theme

---
+++ site/_static/js/tracker.js
@@ -0,0 +1,26 @@
+var Tracker = Tracker||(function () {
+ 
+    var trackLinkedIn = function() {
+        (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);
+    }
+ 
+    return {
+        page: function() {
+            gtag('event', 'page_view', {
+                page_title: document.title,
+                page_location: window.location.href,
+                page_path: window.location.pathname
+            });
+            fbq('track', 'PageView');
+            twq('track','PageView');
+            trackLinkedIn();
+        }
+    }
+ })();
--- site/content/user-tracking.md
+++
@@ -1,7 +0,0 @@
-+++
-title = "User Tracking"
-description = "Page about user tracking"
-date = 2022-01-23T00:37:23Z
-+++
-
-Page About user tracking
